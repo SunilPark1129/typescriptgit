8c6a9b634d82bd329e709d9204d200089785126b
Sunil Park <sunilpark1129@gmail.com>
2023-05-31 07:47:38 -0400

    Test validatable

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,15 @@
 export default function Main() {
-  return <div>Main</div>;
+  let num: number;
+  const arr: string[] = [];
+
+  arr.push("4");
+  arr.push("2");
+  arr.push("1");
+  return (
+    <div>
+      {arr.map((x) => (
+        <p>{x}</p>
+      ))}
+    </div>
+  );
 }
