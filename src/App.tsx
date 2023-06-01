500c2b81dead7d47d87c8ac0e6b34e2633c24f23
Sunil Park <sunilpark1129@gmail.com>
2023-06-01 17:27:19 -0400

    Added fetch

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Functions from "./components/Functions";
 import Utility from "./components/Utility";
 import Generics from "./components/Generics";
 import ReactState from "./components/ReactState";
+import Fetch from "./components/Fetch";
 function App() {
   return (
     <div>
@@ -13,6 +14,7 @@ function App() {
       <Utility />
       <Generics />
       <ReactState />
+      <Fetch />
     </div>
   );
 }
---
+++ src/components/Fetch.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+type PostProps = {
+  url: string;
+  artist?: string;
+};
+
+async function getPosts(): Promise<PostProps> {
+  const res = await fetch("https://api.catboys.com/img");
+  const data = await res.json();
+  return data;
+}
+
+export default function Fetch() {
+  const [data, setData] = useState<string | null>(null);
+
+  async function requestFetch() {
+    const res = await getPosts();
+    setData(res.url);
+  }
+  return (
+    <div>
+      <button onClick={requestFetch}>Click</button>
+      <div>
+        {data && (
+          <img
+            src={data}
+            alt="img"
+            width={200}
+            height={200}
+            style={{ objectFit: "cover" }}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
