2e6156cb4c1f4ed3eaff1102ab3143d0c46ca3bb
Sunil Park <sunilpark1129@gmail.com>
2023-06-01 20:53:31 -0400

    reset train

--- src/components/GitTrain.tsx
+++ src/components/GitTrain.tsx
@@ -1,3 +1,26 @@
 export default function GitTrain() {
-  return <div>GitTrain</div>;
+  type Units = {
+    id: number;
+    name: string;
+    price: number;
+    type: string;
+  };
+
+  const unit: Units = {
+    id: 0,
+    name: "car",
+    price: 31000,
+    type: "car",
+  };
+
+  console.log(unit);
+
+  return (
+    <div>
+      {Object.entries(unit).map(([key, value]) => (
+        <p key={key}>{`${key}: ${value}`}</p>
+      ))}
+      <p>I removed 8 commits</p>
+    </div>
+  );
 }
